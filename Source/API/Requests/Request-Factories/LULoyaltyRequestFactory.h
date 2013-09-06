3f68672031036b488817f4a15e9b63be9fb34093
Costa Walcott <costa@thelevelup.com>
2013-09-06 15:26:39 -0400

    Add some quick docs for LULoyaltyRequestFactory

--- Source/API/Requests/Request-Factories/LULoyaltyRequestFactory.h
+++ Source/API/Requests/Request-Factories/LULoyaltyRequestFactory.h
@@ -1,7 +1,17 @@
+/**
+ `LULoyaltyRequestFactory` is used to build requests related to loyalty.
+ */
 @class LUAPIRequest;
 
 @interface LULoyaltyRequestFactory : NSObject
 
+/**
+ Builds a request to retrieve the current user's loyalty at a specific merchant.
+
+ When successful, an `LULoyalty` instance will be returned.
+
+ @param merchantID The ID of the merchant whose loyalty will be requested.
+ */
 + (LUAPIRequest *)requestForLoyaltyForMerchantWithID:(NSNumber *)merchantID;
 
 @end
